05eeb5ab5a81fb50788636a71dfd99a5f603448f
xXHachimanXx <globaldep90@gmail.com>
2021-07-06 20:38:58 -0300

    Adding code from SECOND class

--- codebank/main.go
+++ codebank/main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/infrastructure/grpc/server"
 	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/infrastructure/kafka"
 	repository "github.com/xXHachimanXx/Imersao-FullCycle-3.0/infrastructure/reposiroty"
 	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/usecase"
@@ -32,40 +34,52 @@ import (
 // 	}
 // }
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
 func main() {
 	db := setupDb()
 	defer db.Close()
+	producer := setupKafkaProducer()
+	processTransactionUseCase := setupTransactionUseCase(db, producer)
+	serveGrpc(processTransactionUseCase)
+}
 
+func setupTransactionUseCase(db *sql.DB, producer kafka.KafkaProducer) usecase.UseCaseTransaction {
+	transactionRepository := repository.NewTransactionRepositoryDb(db)
+	useCase := usecase.NewUseCaseTransaction(transactionRepository)
+	useCase.KafkaProducer = producer
+	return useCase
 }
 
 func setupKafkaProducer() kafka.KafkaProducer {
 	producer := kafka.NewKafkaProducer()
 	producer.SetupProducer(os.Getenv("KafkaBootstrapServers"))
-
 	return producer
-}
-
-func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
-	transactionRepository := repository.NewTransactionRepositoryDb(db)
-	useCase := usecase.NewUseCaseTransaction(transactionRepository)
-
-	return useCase
 }
 
 func setupDb() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		"db",
-		"5432",
-		"postgres",
-		"root",
-		"codebank",
+		os.Getenv("host"),
+		os.Getenv("port"),
+		os.Getenv("user"),
+		os.Getenv("password"),
+		os.Getenv("dbname"),
 	)
-
 	db, err := sql.Open("postgres", psqlInfo)
-
 	if err != nil {
 		log.Fatal("error connection to database")
 	}
-
 	return db
+}
+
+func serveGrpc(processTransactionUseCase usecase.UseCaseTransaction) {
+	grpcServer := server.NewGRPCServer()
+	grpcServer.ProcessTransactionUseCase = processTransactionUseCase
+	fmt.Println("Rodando gRPC Server")
+	grpcServer.Serve()
 }
