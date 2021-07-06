cc70114ee72ce55ea20f36063ffd954cfbf1de70
xXHachimanXx <globaldep90@gmail.com>
2021-07-06 13:54:56 -0300

    Add code from SECOND class.

--- codebank/main.go
+++ codebank/main.go
@@ -4,31 +4,45 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
-	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/domain"
+	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/infrastructure/kafka"
 	repository "github.com/xXHachimanXx/Imersao-FullCycle-3.0/infrastructure/reposiroty"
 	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/usecase"
 )
+
+// func main() {
+// 	db := setupDb()
+// 	defer db.Close()
+
+// 	cc := domain.NewCreditCard()
+// 	cc.Number = "1234"
+// 	cc.Name = "Wesley"
+// 	cc.ExpirationYear = 2021
+// 	cc.ExpirationMonth = 7
+// 	cc.CVV = 123
+// 	cc.Limit = 1000
+// 	cc.Balance = 0
+
+// 	repo := repository.NewTransactionRepositoryDb(db)
+// 	err := repo.CreateCreditCard(*cc)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
 
 func main() {
 	db := setupDb()
 	defer db.Close()
 
-	cc := domain.NewCreditCard()
-	cc.Number = "1234"
-	cc.Name = "Wesley"
-	cc.ExpirationYear = 2021
-	cc.ExpirationMonth = 7
-	cc.CVV = 123
-	cc.Limit = 1000
-	cc.Balance = 0
+}
 
-	repo := repository.NewTransactionRepositoryDb(db)
-	err := repo.CreateCreditCard(*cc)
-	if err != nil {
-		fmt.Println(err)
-	}
+func setupKafkaProducer() kafka.KafkaProducer {
+	producer := kafka.NewKafkaProducer()
+	producer.SetupProducer(os.Getenv("KafkaBootstrapServers"))
+
+	return producer
 }
 
 func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
--- codebank/usecase/process_transaction.go
+++ codebank/usecase/process_transaction.go
@@ -1,14 +1,18 @@
 package usecase
 
 import (
+	"encoding/json"
+	"os"
 	"time"
 
 	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/domain"
 	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/dto"
+	"github.com/xXHachimanXx/Imersao-FullCycle-3.0/infrastructure/kafka"
 )
 
 type UseCaseTransaction struct {
 	TransactionRepository domain.TransactionRepository
+	KafkaProducer         kafka.KafkaProducer
 }
 
 func NewUseCaseTransaction(transactionRepository domain.TransactionRepository) UseCaseTransaction {
@@ -29,7 +33,22 @@ func (u UseCaseTransaction) ProcessTransaction(transactionDto dto.Transaction) (
 
 	t := u.newTransaction(transactionDto, ccBalanceAndLimit)
 	t.ProcessAndValidate(creditCard)
+
 	err = u.TransactionRepository.SaveTransaction(*t, *creditCard)
+
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
+	transactionDto.ID = t.ID
+	transactionDto.CreatedAt = t.CreatedAt
+	transactionJson, err := json.Marshal(transactionDto)
+
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
+	err = u.KafkaProducer.Publish(string(transactionJson), os.Getenv("KafkaTransactionsTopic"))
 
 	if err != nil {
 		return domain.Transaction{}, err
